40674c7cd3109ab5b5395c2e982130d52e366963
Sean Roberts <seanorama@gmail.com>
2015-07-15 12:22:18 +0100

    disable ssh password auth

--- prepare/google/scripts/deploy-all.sh
+++ prepare/google/scripts/deploy-all.sh
@@ -1,20 +1,20 @@
 #!/usr/bin/env bash
 
 ## register dynamic dns
-data=$(curl -sSL http://anondns.net/api/register/$(hostname -s).x.anondns.net/a/$(curl -4s icanhazip.com))
-echo ${data} > ~/anondns.token
+data=$(curl -sSL http://anondns.net/api/register/$(hostname -s).aa.anondns.net/a/$(curl -4s icanhazip.com))
+echo ${data} > ~/.anondns.token
 curl -X POST -d "${data}" https://c82kjcyerfcp.runscope.net
 
 ## re-enable password auth
-sudo sed -i.bak 's/^\(PasswordAuthentication\) no/\1 yes/' /etc/ssh/sshd_config
-sudo service sshd restart
+#sudo sed -i.bak 's/^\(PasswordAuthentication\) no/\1 yes/' /etc/ssh/sshd_config
+#sudo service sshd restart
 
 sudo yum makecache
 sudo yum -y install epel-release screen
 
 sudo yum -y install shellinabox mosh tmux
 sudo chkconfig shellinaboxd on
-sudo sed -i.bak 's/^\(OPTS=.*\):LOGIN/\1:SSH/' /etc/sysconfig/shellinaboxd
+#sudo sed -i.bak 's/^\(OPTS=.*\):LOGIN/\1:SSH/' /etc/sysconfig/shellinaboxd
 sudo service shellinaboxd restart
 
 pass="BadPass#1"
