7cd51ffd122aef9590ef76beed465a3a625771b9
Sean Roberts <seanorama@gmail.com>
2015-09-07 08:55:52 +0100

    Update setup.sh

--- security/setup.sh
+++ security/setup.sh
@@ -1,17 +1,16 @@
-#!/usr/bin/bash
+#!/usr/bin/env bash
 
 ## for prepping a 1-node cluster for the security masterclass
 
-## magic, don't touch
-__dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-__file="${__dir}/$(basename "${BASH_SOURCE[0]}")"
-__base="$(basename ${__file} .sh)"
-source ${__dir}/../ambari_functions.sh
+sudo yum -y -q install git
+cd
+curl -sSL https://raw.githubusercontent.com/seanorama/ambari-bootstrap/master/extras/deploy/install-ambari-bootstrap.sh | bash
+source ~/ambari-bootstrap/extras/ambari_functions.sh
 
 ${__dir}/deploy/prep-hosts.sh
 
 export ambari_services="KNOX YARN ZOOKEEPER TEZ PIG SLIDER MAPREDUCE2 HIVE HDFS HBASE"
-${__dir}/deploy/deploy-hdp.sh
+"${__dir}/deploy/deploy-hdp.sh"
 
 source ${__dir}/ambari_functions.sh
 ambari-configs
@@ -21,6 +20,10 @@ echo export ambari_pass=BadPass#1 > ~/.ambari.conf; chmod 600 ~/.ambari.conf
 echo export ambari_pass=BadPass#1 > ~/ambari-bootstrap/extras/.ambari.conf; chmod 660 ~/ambari-bootstrap/extras/.ambari.conf
 source ${__dir}/ambari_functions.sh
 ambari-configs
+
+mirror_host="${mirror_host:-mc-teacher1.$(hostname -d)}"
+mirror_host_ip=$(ping -w 1 ${mirror_host} | awk 'NR==1 {print $3}' | sed 's/[()]//g')
+echo "${mirror_host_ip} mirror.hortonworks.com ${mirror_host} mirror" | sudo tee -a /etc/hosts
 
 sudo mkdir -p /app; sudo chown ${USER}:users /app; sudo chmod g+wx /app
 
