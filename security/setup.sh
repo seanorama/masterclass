ca121fbc05f9586a602bed155fbe330198b77dbb
Sean Roberts <seanorama@gmail.com>
2015-10-28 20:35:37 +0000

    security fixes

--- security/setup.sh
+++ security/setup.sh
@@ -10,6 +10,7 @@ source ~/ambari-bootstrap/extras/ambari_functions.sh
 ${__dir}/deploy/prep-hosts.sh
 
 export ambari_services="KNOX YARN ZOOKEEPER TEZ PIG SLIDER MAPREDUCE2 HIVE HDFS HBASE SQOOP FLUME OOZIE"
+export custom_repos=true
 "${__dir}/deploy/deploy-hdp.sh"
 sleep 30
 
@@ -19,6 +20,11 @@ echo "export ambari_pass=BadPass#1" > ~/ambari-bootstrap/extras/.ambari.conf; ch
 source ${__dir}/ambari_functions.sh
 ambari-configs
 ambari_wait_request_complete 1
+
+## details of my ad host
+ad_host="${ad_host:-activedirectory.$(hostname -d)}"
+ad_host_ip=$(ping -w 1 ${ad_host} | awk 'NR==1 {print $3}' | sed 's/[()]//g')
+echo "${ad_host_ip} activedirectory.hortonworks.com ${ad_host} activedirectory" | sudo tee -a /etc/hosts
 
 ## Generic setup
 sudo chkconfig mysqld on; sudo service mysqld start
@@ -36,5 +42,4 @@ mirror_host="${mirror_host:-mc-teacher1.$(hostname -d)}"
 mirror_host_ip=$(ping -w 1 ${mirror_host} | awk 'NR==1 {print $3}' | sed 's/[()]//g')
 echo "${mirror_host_ip} mirror.hortonworks.com ${mirror_host} mirror admin admin.hortonworks.com" | sudo tee -a /etc/hosts
 sudo mkdir -p /app; sudo chown ${USER}:users /app; sudo chmod g+wx /app
-
 
