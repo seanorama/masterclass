6f547281ae8a0b4471f5c58a09914763070238d9
Sean Roberts <seanorama@gmail.com>
2016-01-25 21:19:35 +0000

    script for deploying nifi & zeppelin

---
+++ single-view/setup.sh
@@ -0,0 +1,60 @@
+#!/usr/bin/env bash
+set -o xtrace
+
+export TERM=xterm
+
+yum makecache
+yum -y -q install git epel-release ntpd
+
+el_version=$(sed 's/^.\+ release \([.0-9]\+\).*/\1/' /etc/redhat-release | cut -d. -f1)
+case ${el_version} in
+  "6")
+    sed -i "s/mirrorlist=https/mirrorlist=http/" /etc/yum.repos.d/epel.repo || true
+  ;;
+  "7")
+    rpm -Uvh http://dev.mysql.com/get/mysql-community-release-el7-5.noarch.rpm
+  ;;
+esac
+
+cd
+curl -sSL https://raw.githubusercontent.com/seanorama/ambari-bootstrap/master/extras/deploy/install-ambari-bootstrap.sh | bash
+#export ambari_repo=http://s3.amazonaws.com/dev.hortonworks.com/ambari/centos7/2.x/BUILDS/2.1.3.0-291/ambaribn.repo
+#export ambari_repo=http://s3.amazonaws.com/dev.hortonworks.com/ambari/centos${el_version}/2.x/BUILDS/2.2.0.0-1291/ambaribn.repo
+~/ambari-bootstrap/ambari-bootstrap.sh
+sleep 10
+
+## Ambari Server specific tasks
+if [ "${install_ambari_server}" = "true" ]; then
+    bash -c "nohup ambari-server start" || true
+    yum -y -q install mysql-connector-java jq python-argparse python-configobj
+    ambari-server setup --jdbc-db=mysql --jdbc-driver=/usr/share/java/mysql-connector-java.jar
+    ambari_pass=admin source ~/ambari-bootstrap/extras/ambari_functions.sh
+    ambari-change-pass admin admin ${ambari_pass}
+
+    git clone https://github.com/hortonworks-gallery/ambari-zeppelin-service.git /var/lib/ambari-server/resources/stacks/HDP/2.3/services/ZEPPELIN
+    sed -i.bak '/dependencies for all/a \    "ZEPPELIN_MASTER-START": ["NAMENODE-START", "DATANODE-START"],' /var/lib/ambari-server/resources/stacks/HDP/2.3/role_command_order.json
+    git clone https://github.com/abajwa-hw/ambari-nifi-service.git   /var/lib/ambari-server/resources/stacks/HDP/2.3/services/NIFI
+
+    service ambari-server restart
+    service ambari-agent restart
+
+    if [ "${deploy}" = "true" ]; then
+        sleep 60
+        export ambari_password="${ambari_pass}"
+        export cluster_name=${stack}
+        export host_count=${host_count:-skip}
+        cd ~/ambari-bootstrap/deploy
+
+        export ambari_services="AMBARI_METRICS FLUME HBASE HDFS HIVE MAHOUT MAPREDUCE2 OOZIE PIG SLIDER SPARK SQOOP TEZ YARN ZOOKEEPER ZEPPELIN NIFI"
+        ./deploy-recommended-cluster.bash
+        cd ~
+        sleep 5
+
+        source ~/ambari-bootstrap/extras/ambari_functions.sh
+        ambari-configs
+        ambari_wait_request_complete 1
+    fi
+fi
+
+exit 0
+
