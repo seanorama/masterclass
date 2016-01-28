f1f21627878b13de9e9804963549503582564ccb
Sean Roberts <seanorama@gmail.com>
2016-01-28 22:19:02 +0000

    ambari init script bug workarounds

--- single-view/setup.sh
+++ single-view/setup.sh
@@ -32,10 +32,7 @@ if [ "${install_ambari_server}" = "true" ]; then
 
     git clone https://github.com/abajwa-hw/ambari-nifi-service.git   /var/lib/ambari-server/resources/stacks/HDP/2.3/services/NIFI
 
-    nohup sh -c "ambari-server stop 2>&1 > /dev/null"
-    sleep 5
-    nohup sh -c "ambari-server start 2>&1 > /dev/null"
-    sleep 5
+    nohup service ambari-server restart
 
     yum -y -q install mysql-connector-java jq python-argparse python-configobj
     ambari-server setup --jdbc-db=mysql --jdbc-driver=/usr/share/java/mysql-connector-java.jar
