b0d707ed39eecae48738970ec16ee6caa8748b6e
Sean Roberts <seanorama@gmail.com>
2016-02-08 20:28:30 +0000

    update to support solr & custom external script

--- security-advanced/setup.sh
+++ security-advanced/setup.sh
@@ -1,10 +1,15 @@
 #!/usr/bin/env bash
 set -o xtrace
 
+export HOME=${HOME:-/root}
 export TERM=xterm
+export ambari_pass=${ambari_pass:-BadPass#1}
+export ambari_server_custom_script=${ambari_server_custom_script:-~/ambari-bootstrap/ambari-extras.sh}
+
+cd
 
 yum makecache
-yum -y -q install git epel-release ntpd
+yum -y -q install git epel-release ntpd screen mysql-connector-java jq python-argparse python-configobj
 
 el_version=$(sed 's/^.\+ release \([.0-9]\+\).*/\1/' /etc/redhat-release | cut -d. -f1)
 case ${el_version} in
@@ -16,28 +21,61 @@ case ${el_version} in
   ;;
 esac
 
-cd
 curl -sSL https://raw.githubusercontent.com/seanorama/ambari-bootstrap/master/extras/deploy/install-ambari-bootstrap.sh | bash
-#export ambari_repo=http://s3.amazonaws.com/dev.hortonworks.com/ambari/centos7/2.x/BUILDS/2.1.3.0-291/ambaribn.repo
-#export ambari_repo=http://s3.amazonaws.com/dev.hortonworks.com/ambari/centos${el_version}/2.x/BUILDS/2.2.0.0-1291/ambaribn.repo
+
 ~/ambari-bootstrap/ambari-bootstrap.sh
-sleep 10
 
 ## Ambari Server specific tasks
 if [ "${install_ambari_server}" = "true" ]; then
     bash -c "nohup ambari-server start" || true
-    yum -y -q install mysql-connector-java jq python-argparse python-configobj
+
+    sleep 60
+
     ambari-server setup --jdbc-db=mysql --jdbc-driver=/usr/share/java/mysql-connector-java.jar
     ambari_pass=admin source ~/ambari-bootstrap/extras/ambari_functions.sh
     ambari-change-pass admin admin ${ambari_pass}
 
     if [ "${deploy}" = "true" ]; then
-        sleep 60
-        export ambari_password="${ambari_pass}"
-        export cluster_name=${stack}
-        export host_count=${host_count:-skip}
+
         cd ~/ambari-bootstrap/deploy
-        #export ambari_services="ACCUMULO AMBARI_METRICS FALCON FLUME HBASE HDFS HIVE KNOX MAHOUT MAPREDUCE2 OOZIE PIG SLIDER SPARK SQOOP STORM TEZ YARN ZOOKEEPER"
+
+        ## various configuration changes for demo environments, and fixes to defaults
+cat << EOF > configuration-custom.json
+{
+  "configurations" : {
+    "yarn-site": {
+        "yarn.scheduler.minimum-allocation-vcores": "1",
+        "yarn.scheduler.maximum-allocation-vcores": "1",
+        "yarn.scheduler.minimum-allocation-mb": "256",
+        "yarn.scheduler.maximum-allocation-mb": "2048"
+    },
+    "hive-site": {
+        "hive.support.concurrency": "true",
+        "hive.enforce.bucketing": "true",
+        "hive.exec.dynamic.partition.mode": "nonstrict",
+        "hive.txn.manager": "org.apache.hadoop.hive.ql.lockmgr.DbTxnManager",
+        "hive.compactor.initiator.on": "true",
+        "hive.compactor.worker.threads": "1"
+    },
+    "solr-env": {
+        "solr.port": "6083"
+    },
+    "solr-config": {
+        "solr.port": "6083",
+        "solr.datadir": "/opt/ranger_audit_server",
+        "solr.download.location": "HDPSEARCH",
+        "solr.znode": "/ranger_audits",
+        "solr.cloudmode": "true"
+    }
+  }
+}
+EOF
+
+        export ambari_services="${ambari_services:-"HDFS MAPREDUCE2 PIG YARN HIVE ZOOKEEPER SOLR AMBARI_METRICS HBASE"}"
+        export ambari_password="${ambari_pass}"
+        export cluster_name=${stack:-mycluster}
+        export host_count=${host_count:-skip}
+
         ./deploy-recommended-cluster.bash
         cd ~
         sleep 5
