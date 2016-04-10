b5ca2cfce2c2346d7c9f01079bf7f23aee9af82e
Sean Roberts <seanorama@gmail.com>
2016-04-10 09:55:00 +0100

    move demo setup to script for users to run

--- single-view/scripts/00_prep_host.sh
+++ single-view/scripts/00_prep_host.sh
@@ -11,89 +11,30 @@ yum -y -q install autoconf python-crypto python-devel unzip gcc-c++ git python-a
 
 git clone -b develop https://github.com/seanorama/ambari-bootstrap
 ~/ambari-bootstrap/extras/deploy/prep-hosts.sh
-echo BadPass#1 > ~/.ambari.conf
-chmod 600 ~/.ambari.conf
 
-exit 0
+git clone https://github.com/seanorama/masterclass
 
-cat << EOF > configuration-custom.json
-{
-  "configurations" : {
-    "hdfs-site": {
-        "dfs.replication": "1"
-    },
-    "yarn-site": {
-        "yarn.scheduler.minimum-allocation-vcores": "1",
-        "yarn.scheduler.maximum-allocation-vcores": "1",
-        "yarn.scheduler.minimum-allocation-mb": "256",
-        "yarn.scheduler.maximum-allocation-mb": "2048"
-    },
-    "hive-site": {
-        "hive.support.concurrency": "true",
-        "hive.enforce.bucketing": "true",
-        "hive.exec.dynamic.partition.mode": "nonstrict",
-        "hive.txn.manager": "org.apache.hadoop.hive.ql.lockmgr.DbTxnManager",
-        "hive.compactor.initiator.on": "true",
-        "hive.compactor.worker.threads": "1"
-    },
-    "solr-config": {
-        "solr.download.location": "HDPSEARCH",
-        "solr.znode": "/solr",
-        "solr.cloudmode": "true"
-    },
-    "core-site": {
-        "hadoop.proxyuser.HTTP.groups" : "users,hadoop-users",
-        "hadoop.proxyuser.HTTP.hosts" : "*",
-        "hadoop.proxyuser.hbase.groups" : "users,hadoop-users",
-        "hadoop.proxyuser.hbase.hosts" : "*",
-        "hadoop.proxyuser.hcat.groups" : "users,hadoop-users",
-        "hadoop.proxyuser.hcat.hosts" : "*",
-        "hadoop.proxyuser.hive.groups" : "users,hadoop-users",
-        "hadoop.proxyuser.hive.hosts" : "*",
-        "hadoop.proxyuser.knox.groups" : "users,hadoop-users",
-        "hadoop.proxyuser.knox.hosts" : "*",
-        "hadoop.proxyuser.oozie.groups" : "users",
-        "hadoop.proxyuser.oozie.hosts" : "*",
-        "hadoop.proxyuser.root.groups" : "users,hadoop-users",
-        "hadoop.proxyuser.root.hosts" : "*"
-    }
-  }
-}
-EOF
+(
+cd /tmp
+wget http://hortonworks-masterclass.s3.amazonaws.com/single-view/data.zip
+unzip data.zip
+)
 
-        export ambari_password="${ambari_pass}"
-        export cluster_name=${stack:-mycluster}
-        export host_count=${host_count:-skip}
+yum -y -q install postgresql-server
+echo "host all all 127.0.0.1/32 md5" >> /var/lib/pgsql/data/pg_hba.conf
 
-        sleep 60
+service postgresql start
+chkconfig postgresql on
+sleep 5
 
-        #export ambari_services="HDFS HIVE MAPREDUCE2 PIG SLIDER SPARK TEZ YARN ZOOKEEPER ZEPPELIN NIFI"
-        ./deploy-recommended-cluster.bash
-        cd ~
-        sleep 5
+git clone https://github.com/abajwa-hw/single-view-demo
+sudo -i -u postgres psql -c "create database contoso;"
+sudo -i -u postgres psql -c "CREATE USER zeppelin WITH PASSWORD 'zeppelin';"
+sudo -i -u postgres psql -c "GRANT ALL PRIVILEGES ON DATABASE contoso to zeppelin;"
+sudo -i -u postgres psql -c "\du"
+export PGPASSWORD=zeppelin
+psql -U zeppelin -d contoso -h localhost -f ~/single-view-demo/contoso-psql.sql
+psql -U zeppelin -d contoso -h localhost -c "\dt"
 
-        source ~/ambari-bootstrap/extras/ambari_functions.sh
-        ambari-configs
-        ambari_wait_request_complete 1
+echo "zeppelin  ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
 
-        usermod -a -G users ${USER}
-        ~/ambari-bootstrap/extras/onboarding.sh
-
-        #useradd admin
-        #usermod -a -G users admin
-        #sudo -u hdfs hadoop fs -mkdir /user/admin
-        #sudo -u hdfs hadoop fs -chown /user/admin
-
-        #config_proxyuser=true ~/ambari-bootstrap/extras/ambari-views/create-views.sh
-
-        #yum install -y lucidworks-hdpsearch
-        #sudo -u hdfs hadoop fs -mkdir /user/solr
-        #sudo -u hdfs hadoop fs -chown solr /user/solr
-        #chown -R solr:solr /opt/lucidworks-hdpsearch/solr
-        #echo ZK_HOST="localhost:2181" >> /opt/lucidworks-hdpsearch/solr/bin/solr.in.sh
-        #echo SOLR_MODE=solrcloud >> /opt/lucidworks-hdpsearch/solr/bin/solr.in.sh
-        #service solr start
-        #chkconfig solr on
-
-    fi
-fi
--- single-view/scripts/20_setup_demo.sh
+++ single-view/scripts/20_setup_demo.sh
@@ -1,14 +1,37 @@
 #!/usr/bin/env bash
 
+cd ~
+
+wget https://jdbc.postgresql.org/download/postgresql-9.4.1207.jar -P /usr/hdp/current/sqoop-client/lib
+
 UID_MIN=$(awk '$1=="UID_MIN" {print $2}' /etc/login.defs)
 users="${users:-$(getent passwd|awk -v UID_MIN="${UID_MIN}" -F: '$3>=UID_MIN{print $1}')}"
-
 dfs_cmd="sudo sudo -u hdfs hadoop fs"
 for user in ${users}; do
-    if ! ${dfs_cmd} -stat /user/${user}; then
+    usermod -a -G users "${user}"
+    if ! ${dfs_cmd} -stat /user/${user} 2> /dev/null; then
       ${dfs_cmd} -mkdir -p "/user/${user}"
-      ${dfs_cmd} -chown "${user}" "/user/${user}" &
+      ${dfs_cmd} -chown "${user}" "/user/${user}"
     fi
 done
 
+(
+cd /tmp
+wget http://hortonworks-masterclass.s3.amazonaws.com/single-view/data.zip
+unzip data.zip
+)
+
+echo "host all all 127.0.0.1/32 md5" >> /var/lib/pgsql/data/pg_hba.conf
+service postgresql reload
+
+git clone https://github.com/abajwa-hw/single-view-demo
+sudo -i -u postgres psql -c "create database contoso;"
+sudo -i -u postgres psql -c "CREATE USER zeppelin WITH PASSWORD 'zeppelin';"
+sudo -i -u postgres psql -c "GRANT ALL PRIVILEGES ON DATABASE contoso to zeppelin;"
+sudo -i -u postgres psql -c "\du"
+export PGPASSWORD=zeppelin
+psql -U zeppelin -d contoso -h localhost -f ~/single-view-demo/contoso-psql.sql
+psql -U zeppelin -d contoso -h localhost -c "\dt"
+
+echo "zeppelin  ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
 
