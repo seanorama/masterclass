5d82f07954ba4097a07fc0a8ad33dc0667ee915e
Sean Roberts <seanorama@gmail.com>
2016-05-12 12:56:27 -0700

    no message

--- amazon-linux/setup.sh
+++ amazon-linux/setup.sh
@@ -20,7 +20,7 @@ export ambari_server_custom_script='\
     ; ambari-agent restart \
     ; yum -y -q install smartsense-hst \
     ; URL="https://gist.githubusercontent.com/seanorama/bbe936cff511d8e5b98f1c8b6c155f55/raw/5446e0747e0a6e99b49c881e23a70748aec97b19/security.py.diff" \
-    curl -sSL "${URL}" | patch -b /usr/hdp/share/hst/hst-agent/lib/hst_agent/security.py
+    ; curl -sSL "${URL}" | patch -b /usr/hdp/share/hst/hst-agent/lib/hst_agent/security.py
 '
 
 git clone -b feature/amazon-linux http://github.com/seanorama/ambari-bootstrap
