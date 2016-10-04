e08832e3ea301ffff26785eaba92f25769a62098
Sean Roberts <seanorama@gmail.com>
2016-10-04 10:55:32 -0700

    install rackerlabs/recap

--- security-advanced/setup.sh
+++ security-advanced/setup.sh
@@ -13,6 +13,27 @@ cd
 yum makecache
 yum -y -q install git epel-release ntpd screen mysql-connector-java jq python-argparse python-configobj ack
 
+########
+########
+## Install rackerlabs/recap to gather metrics
+yum -y -q install git bc elinks net-tools sysstat iotop
+git clone https://github.com/rackerlabs/recap.git
+cd recap; make install; cd
+
+opts="USESAR USESARR USESARQ USEPSTREE USENETSTATSUM USEDF USESLAB USEFDISK"
+for opt in ${opts}
+do
+  sed -i "s/${opt}=no/${opt}=yes/" /etc/recap
+done
+unset opts
+
+recap
+echo '*/5 * * * * root /usr/sbin/recap' >> /etc/cron.d/recap
+########
+########
+
+#ad_ip=172.30.0.78
+#echo "${ad_ip} ad01.lab.hortonworks.net ad01" | sudo tee -a /etc/hosts
 
 curl -sSL https://raw.githubusercontent.com/seanorama/ambari-bootstrap/master/extras/deploy/install-ambari-bootstrap.sh | bash
 
