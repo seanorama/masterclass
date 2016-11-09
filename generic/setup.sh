bef1eedd9523da47ec7ff16b83fb108eca67897b
Sean Roberts <seanorama@gmail.com>
2016-11-09 12:44:41 +0000

    update generic to function for 2.5

--- generic/setup.sh
+++ generic/setup.sh
@@ -3,23 +3,26 @@ set -o xtrace
 
 export HOME=${HOME:-/root}
 export TERM=xterm
-export ambari_pass=${ambari_pass:-BadPass#1}
-#export ambari_server_custom_script=${ambari_server_custom_script:-~/ambari-bootstrap/ambari-extras.sh}
+: ${ambari_pass:="BadPass#1"}
+ambari_password="${ambari_pass}"
+: ${ambari_services:="HDFS MAPREDUCE2 PIG YARN HIVE ZOOKEEPER AMBARI_METRICS SLIDER AMBARI_INFRA LOGSEARCH TEZ"}
+: ${install_ambari_server:=true}
+: ${ambari_stack_version:=2.5}
+cluster_name=${stack:-mycluster}
+
+: ${install_ambari_server:=true}
+: ${ambari_stack_version:=2.5}
+: ${host_count:=skip}
+
+: ${recommendation_strategy:="ALWAYS_APPLY_DONT_OVERRIDE_CUSTOM_VALUES"}
+
+export install_ambari_server ambari_pass host_count ambari_services
+export ambari_password cluster_name recommendation_strategy
 
 cd
 
 yum makecache
-yum -y -q install git epel-release ntpd screen mysql-connector-java jq python-argparse python-configobj
-
-el_version=$(sed 's/^.\+ release \([.0-9]\+\).*/\1/' /etc/redhat-release | cut -d. -f1)
-case ${el_version} in
-  "6")
-    sed -i "s/mirrorlist=https/mirrorlist=http/" /etc/yum.repos.d/epel.repo || true
-  ;;
-  "7")
-    rpm -Uvh http://dev.mysql.com/get/mysql-community-release-el7-5.noarch.rpm
-  ;;
-esac
+yum -y -q install git epel-release ntpd screen mysql-connector-java jq python-argparse python-configobj ack
 
 curl -sSL https://raw.githubusercontent.com/seanorama/ambari-bootstrap/master/extras/deploy/install-ambari-bootstrap.sh | bash
 
@@ -29,13 +32,13 @@ curl -sSL https://raw.githubusercontent.com/seanorama/ambari-bootstrap/master/ex
 
 ## Ambari Server specific tasks
 if [ "${install_ambari_server}" = "true" ]; then
-    bash -c "nohup ambari-server start" || true
+    bash -c "nohup ambari-server restart" || true
 
     sleep 60
 
-    ambari-server setup --jdbc-db=mysql --jdbc-driver=/usr/share/java/mysql-connector-java.jar
     ambari_pass=admin source ~/ambari-bootstrap/extras/ambari_functions.sh
     ambari_change_pass admin admin ${ambari_pass}
+    sleep 1
 
     if [ "${deploy}" = "true" ]; then
 
@@ -45,61 +48,33 @@ if [ "${install_ambari_server}" = "true" ]; then
 cat << EOF > configuration-custom.json
 {
   "configurations" : {
-    "yarn-site": {
-        "yarn.scheduler.minimum-allocation-vcores": "1",
-        "yarn.scheduler.maximum-allocation-vcores": "1",
-        "yarn.scheduler.minimum-allocation-mb": "256",
-        "yarn.acl.enable" : "true",
-        "yarn.scheduler.maximum-allocation-mb": "2048"
+    "core-site": {
+        "hadoop.proxyuser.root.users" : "admin",
+        "fs.trash.interval": "4320"
     },
     "hdfs-site": {
       "dfs.namenode.safemode.threshold-pct": "0.99"
+    },
+    "hive-interactive-env": {
+        "enable_hive_interactive": "true",
+        "llap_queue_capacity": "75"
+    },
+    "hive-site": {
+        "hive.exec.compress.output": "true",
+        "hive.merge.mapfiles": "true",
+        "hive.server2.tez.initialize.default.sessions": "true"
     },
     "mapred-site": {
         "mapreduce.job.reduce.slowstart.completedmaps": "0.7",
         "mapreduce.map.output.compress": "true",
         "mapreduce.output.fileoutputformat.compress": "true"
     },
-    "hive-site": {
-        "hive.exec.compress.output": "true",
-        "hive.merge.mapfiles": "true",
-        "hive.server2.tez.initialize.default.sessions": "true",
-        "hive.support.concurrency": "true",
-        "hive.enforce.bucketing": "true",
-        "hive.exec.dynamic.partition.mode": "nonstrict",
-        "hive.txn.manager": "org.apache.hadoop.hive.ql.lockmgr.DbTxnManager",
-        "hive.compactor.initiator.on": "true",
-        "hive.compactor.worker.threads": "1"
-    },
-    "hive-interactive-env": {
-        "enable_hive_interactive": "true",
-        "llap_queue_capacity": "75"
-    },
-    "core-site": {
-        "hadoop.proxyuser.HTTP.groups" : "users,hadoop-users",
-        "hadoop.proxyuser.HTTP.hosts" : "*",
-        "hadoop.proxyuser.hbase.groups" : "users,hadoop-users",
-        "hadoop.proxyuser.hbase.hosts" : "*",
-        "hadoop.proxyuser.hcat.groups" : "users,hadoop-users",
-        "hadoop.proxyuser.hcat.hosts" : "*",
-        "hadoop.proxyuser.hive.groups" : "users,hadoop-users",
-        "hadoop.proxyuser.hive.hosts" : "*",
-        "hadoop.proxyuser.knox.groups" : "users,hadoop-users",
-        "hadoop.proxyuser.knox.hosts" : "*",
-        "hadoop.proxyuser.oozie.groups" : "users",
-        "hadoop.proxyuser.oozie.hosts" : "*",
-        "hadoop.proxyuser.root.groups" : "users,hadoop-users",
-        "hadoop.proxyuser.root.hosts" : "*",
-        "fs.trash.interval": "4320"
+    "yarn-site": {
+        "yarn.acl.enable" : "true"
     }
   }
 }
 EOF
-
-        export ambari_password="${ambari_pass}"
-        export cluster_name=${stack:-mycluster}
-        export host_count=${host_count:-skip}
-        : ${recommendation_strategy:="ALWAYS_APPLY_DONT_OVERRIDE_CUSTOM_VALUES"}
 
         ./deploy-recommended-cluster.bash
         cd ~
@@ -108,9 +83,16 @@ EOF
         source ~/ambari-bootstrap/extras/ambari_functions.sh
         ambari_configs
         ambari_wait_request_complete 1
+        cd ~
+        sleep 10
 
         usermod -a -G users ${USER}
-        ~/ambari-bootstrap/extras/onboarding.sh
-
+        useradd -a -G users admin
+        echo "${ambari_pass}" | passwd admin --stdin
+        sudo sudo -u hdfs bash -c "
+            hadoop fs -mkdir /user/admin;
+            hadoop fs -chown admin /user/admin;
+            hdfs dfsadmin -refreshUserToGroupsMappings"
     fi
 fi
+
