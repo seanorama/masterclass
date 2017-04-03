b126b374374f8fa34febd5a064d74ab80e193c73
Sean Roberts <seanorama@gmail.com>
2017-04-03 12:08:27 +0200

    fix paths and bunzip

--- ranger-atlas/Scripts/load-secgovdemo-hortoniabank-files.sh
+++ ranger-atlas/Scripts/load-secgovdemo-hortoniabank-files.sh
@@ -3,7 +3,9 @@
 #sudo su -
 #kinit -kt /etc/security/keytabs/hdfs.headless.keytab hdfs-hortoniabank@FIELD.HORTONWORKS.COM
 
-bunzip ../Data/*.bz2
+cp -a ../Data /tmp
+bunzip2 /tmp/Data/*.bz2
+
 
 sudo sudo -u hdfs bash -c "
   hdfs dfs -mkdir -p /user/admin/hortoniabank_data/us_customers
@@ -13,12 +15,12 @@ sudo sudo -u hdfs bash -c "
   hdfs dfs -mkdir -p /user/admin/hortoniabank_data/claim_savings
   hdfs dfs -mkdir -p /user/admin/hortoniabank_data/provider_summary
 
-  hdfs dfs -put ../Data/us_customers_data.csv /user/admin/hortoniabank_data/us_customers
-  hdfs dfs -put ../Data/ww_customers_data.csv /user/admin/hortoniabank_data/ww_customers
-  hdfs dfs -put ../Data/eu_countries.csv /user/admin/hortoniabank_data/eu_countries
-  hdfs dfs -put ../Data/tax_2015.csv /user/admin/hortoniabank_data/tax_2015
-  hdfs dfs -put ../Data/claim_savings.csv /user/admin/hortoniabank_data/claim_savings
-  hdfs dfs -put ../Data/claims_provider_summary_data.csv /user/admin/hortoniabank_data/provider_summary
+  hdfs dfs -put /tmp/Data/us_customers_data.csv /user/admin/hortoniabank_data/us_customers
+  hdfs dfs -put /tmp/Data/ww_customers_data.csv /user/admin/hortoniabank_data/ww_customers
+  hdfs dfs -put /tmp/Data/eu_countries.csv /user/admin/hortoniabank_data/eu_countries
+  hdfs dfs -put /tmp/Data/tax_2015.csv /user/admin/hortoniabank_data/tax_2015
+  hdfs dfs -put /tmp/Data/claim_savings.csv /user/admin/hortoniabank_data/claim_savings
+  hdfs dfs -put /tmp/Data/claims_provider_summary_data.csv /user/admin/hortoniabank_data/provider_summary
 "
 
 
